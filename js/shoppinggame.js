293ea3cb17eea04760f061381a6c70530ff1412e
jhillTechtonic <julie.hill@techtonic.com>
2020-11-23 17:51:14 -0600

    completed module 3

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -211,10 +211,18 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return (product) => {
+        if (product.id === id) {
+            return true;
+        }
+    }
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor((Math.random() * 20) + 1);
+};
 
 
 const getProduct = (prodList, pId) => {
